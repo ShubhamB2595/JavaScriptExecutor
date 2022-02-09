6b63127b649e0022510b6f495114ae289e51176c
ShubhamB2595 <shubhambhele95@gmail.com>
2022-02-09 17:22:25 +0530

    code ADDED for the method draw border and take screenshot

--- src/main/java/JavaScriptExecutorMain.java
+++ src/main/java/JavaScriptExecutorMain.java
@@ -1,7 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JavaScriptExecutorMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Java Script Executor Project");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+
+        WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+        JavaScriptUtil.drawBorder(logo, driver);
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File srcFile = ts.getScreenshotAs(OutputType.FILE);
+        File trgFile = new File(".\\screenshots\\logo.png");
+        FileUtils.copyFile(srcFile, trgFile);
     }
 }
